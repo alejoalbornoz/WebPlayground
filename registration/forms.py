4eb9f4fccfbc59c583e2258bb648b4929b7a30aa
alejoalbornoz <alejoalbornoz912@gmail.com>
2024-12-23 16:48:45 -0300

    Add: Validation for Email in signup

--- registration/forms.py
+++ registration/forms.py
@@ -8,3 +8,15 @@ class UserCreationFormWithEmail(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+    #Validation for email
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("El email ya esta registrado, prueba con otro")
+        
+        return email
+    
+
+    
