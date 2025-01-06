9d1b63ebc89568d8b1076ba758a05c8c4912e751
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-01-06 18:46:32 -0300

    Test asynchronous

--- messenger/views.py
+++ messenger/views.py
@@ -4,7 +4,7 @@ from .models import Thread
 from django.views.generic import TemplateView 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.http import Http404
+from django.http import Http404, JsonResponse
 # Create your views here.
 
 
@@ -22,3 +22,8 @@ class ThreadDetail(DetailView):
         if self.request.user not in obj.users.all():
             raise Http404()
         return obj
+    
+def add_message(request, pk):
+    print(request.GET)
+    json_response = {"created": False}
+    return JsonResponse(json_response)
