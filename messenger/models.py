c6d8e184cd2c8b4a1cb12469266f86b6bb813915
alejoalbornoz <alejoalbornoz912@gmail.com>
2025-01-09 16:08:16 -0300

    Ordering Thread

--- messenger/models.py
+++ messenger/models.py
@@ -29,8 +29,12 @@ class ThreadManager(models.Manager):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name="threads")
     messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
 
     objects = ThreadManager()
+
+    class Meta:
+        ordering = ["-updated"]
     
 
 def messages_changed(sender, **kwargs):
